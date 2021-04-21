ccb79c0fc44f3b788042bc29acdc8249a02ea042
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-04-21 15:45:14 +0530

    ADD: Method to show contact by city or state

--- main/java/AddressBookSimulator.java
+++ main/java/AddressBookSimulator.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AddressBookSimulator {
     static AddressBookSimulator addressBookSimulator = new AddressBookSimulator();
     ScannerForAddressBook scannerForAddressBook = new ScannerForAddressBook();
-    private Map<Object, AddressBookService> booksMap = new HashMap<>();
+    private Map<String, AddressBookService> booksMap = new HashMap<>();
+    private Map<String, List<List<ContactPOJO>>> cityPersonMap = new HashMap<>();
+    private Map<String, List<List<ContactPOJO>>> statePersonMap = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -14,7 +19,8 @@ public class AddressBookSimulator {
         boolean isExit = false;
         while(!isExit) {
             System.out.println("Select options: \n1.Add Book\n2.AccessBook\n3.Search contact by city/state\n" +
-                                                  "4.Exit");
+                                                  "4.Show the contacts by city\n5.Show the contacts by state\n" +
+                                                  "6.Exit");
             int option = addressBookSimulator.scannerForAddressBook.scannerProvider().nextInt();
             switch(option) {
                 case 1:
@@ -86,6 +92,44 @@ public class AddressBookSimulator {
                             contact.getState().equals(placeName))
                     .findFirst().orElse(null));
         });
+    }
+
+    /**
+     * Show the contacts by city
+     */
+    public void showContactsByCity(){
+        System.out.println("Enter the city name to search contacts");
+        String city = scannerForAddressBook.scannerProvider().nextLine();
+        List<List<ContactPOJO>> listOfCityContactsList = new ArrayList<>();
+        List<ContactPOJO> cityContactList;
+        for(Map.Entry<String, AddressBookService> entry : addressBookSimulator.booksMap.entrySet()) {
+            cityContactList = entry.getValue().getContactsList().stream()
+                    .filter(contact ->
+                            contact.getCity().equals(city))
+                    .collect(Collectors.toList());
+            listOfCityContactsList.add(cityContactList);
+        }
+        addressBookSimulator.cityPersonMap.put(city,listOfCityContactsList);
+        System.out.println(addressBookSimulator.cityPersonMap);
+    }
+
+    /**
+     * Show the contacts by state
+     */
+    public void showContactByState(){
+        System.out.println("Enter the state name to search contacts");
+        String state = scannerForAddressBook.scannerProvider().nextLine();
+        List<List<ContactPOJO>> listOfStateContactsList = new ArrayList<>();
+        List<ContactPOJO> stateContactList;
+        for (Map.Entry<String, AddressBookService> entry : addressBookSimulator.booksMap.entrySet()) {
+            stateContactList = entry.getValue().getContactsList().stream()
+                    .filter(contact ->
+                            contact.getState().equals(state))
+                    .collect(Collectors.toList());
+            listOfStateContactsList.add(stateContactList);
+        }
+        addressBookSimulator.statePersonMap.put(state,listOfStateContactsList);
+        System.out.println(addressBookSimulator.statePersonMap);
     }
 
     /**
