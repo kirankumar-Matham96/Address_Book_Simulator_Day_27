6043cc566bceb3857bd68f002ced03cb118d67d6
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-04-21 13:31:15 +0530

    ADD: Edit method to edit details of person through console

--- main/java/AddressBookService.java
+++ main/java/AddressBookService.java
@@ -32,4 +32,99 @@ public class AddressBookService {
         contactsList.add(contacts);
         System.out.println(contactsList);
     }
+
+    /**
+     * edits the contact details of selected name
+     */
+    public void editDetails() {
+        String name = getName();
+        if (isContactExist(name) == true) {
+            ContactPOJO contact = getContact(name);
+            boolean isExit = false;
+            while (!isExit) {
+                System.out.println("Select option:" + "\n" + "1.first name" + "\n" + "2.last name" +
+                        "\n" + "3.address" + "\n" + "4.city" + "\n" +
+                        "5.state" + "\n" + "6.email" + "\n" + "7.zip" +
+                        "\n" + "8.phone number" + "\n" + "9.exit");
+                switch (scannerForAddressBook.scannerProvider().nextInt()) {
+                    case 1:
+                        System.out.println("Enter new first name to change: ");
+                        contact.setFirstName(scannerForAddressBook.scannerProvider().nextLine());
+                        break;
+                    case 2:
+                        System.out.println("Enter new last name to change: ");
+                        contact.setLastName(scannerForAddressBook.scannerProvider().nextLine());
+                        break;
+                    case 3:
+                        System.out.println("Enter new address to change: ");
+                        contact.setAddress(scannerForAddressBook.scannerProvider().nextLine());
+                        break;
+                    case 4:
+                        System.out.println("Enter new city to change: ");
+                        contact.setCity(scannerForAddressBook.scannerProvider().nextLine());
+                        break;
+                    case 5:
+                        System.out.println("Enter new state to change: ");
+                        contact.setState(scannerForAddressBook.scannerProvider().nextLine());
+                        break;
+                    case 6:
+                        System.out.println("Enter new email to change: ");
+                        contact.setEmail(scannerForAddressBook.scannerProvider().nextLine());
+                        break;
+                    case 7:
+                        System.out.println("Enter new zip to change: ");
+                        contact.setZip(scannerForAddressBook.scannerProvider().nextInt());
+                        break;
+                    case 8:
+                        System.out.println("Enter new phone number to change: ");
+                        contact.setPhoneNumber(scannerForAddressBook.scannerProvider().nextLine());
+                        break;
+                    default:
+                        System.out.println("Thank you!");
+                        System.out.println(contactsList);
+                        isExit = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * asks the user for name and returns it
+     *
+     * @return contact first name / last name
+     */
+    private String getName() {
+        System.out.println("Enter contact name");
+        return scannerForAddressBook.scannerProvider().nextLine();
+    }
+
+    /**
+     * To search the contact
+     *
+     * @param name first or last name of the contact
+     * @return object of the Contact (Contact it self)
+     */
+    private ContactPOJO getContact(String name) {
+        ContactPOJO foundContact = contactsList.stream()
+                .filter(contact ->
+                        contact.getFirstName().equals(name) ||
+                                contact.getLastName().equals(name))
+                .findFirst().orElse(null);
+        return foundContact;
+    }
+
+    /**
+     * To find if contact exists or not
+     *
+     * @param name first or last name of the contact
+     * @return true/false
+     */
+    private boolean isContactExist(String name) {
+        boolean existance;
+        existance = contactsList.stream()
+                .anyMatch(personElement ->
+                        personElement.getFirstName().equals(name) ||
+                                personElement.getLastName().equals(name));
+        return existance;
+    }
 }
--- main/java/AddressBookSimulator.java
+++ main/java/AddressBookSimulator.java
@@ -3,5 +3,6 @@ public class AddressBookSimulator {
         System.out.println("Welcome to address book simulator program!");
         AddressBookService addressBookService = new AddressBookService();
         addressBookService.addContact();
+        addressBookService.editDetails();
     }
 }
